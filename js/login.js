29a81891ea0f53ebdf0f3123320bbf18a989d9a0
Evgeny Yalovik <yalovikea@gmail.com>
2018-07-11 20:38:39 +0300

    Добавлена анимация и скрипты к страницам

---
+++ js/login.js
@@ -0,0 +1,54 @@
+var login = document.querySelector('.user-block__link');
+var modalLogin = document.querySelector('.modal-login');
+var modalLoginClose = document.querySelector('.modal-login__close');
+var form = document.querySelector('.login-form');
+var named = document.getElementById('login-name');
+var password = document.getElementById('login-password');
+var storage = localStorage.getItem('name');
+
+login.addEventListener('click', showLogin);
+form.addEventListener('submit', submitForm);
+modalLoginClose.addEventListener('click', closeLogin);
+window.addEventListener('keydown', closeLoginEscape);
+
+
+function showLogin(EO) {
+  EO.preventDefault();
+  modalLogin.classList.add('modal-show');  
+
+  if (storage) {
+    named.value = storage;
+    password.focus();
+  } else {
+    named.focus();
+  }
+}
+
+function submitForm(EO) {
+  if(!named.value || !password.value) {
+    EO.preventDefault();
+    modalLogin.classList.remove('modal-error');
+    modalLogin.offsetWidth = modalLogin.offsetWidth;
+    modalLogin.classList.add('modal-error');
+  } else {
+    localStorage.setItem('name', named.value);
+  }
+}
+
+function closeLogin(EO) {
+  EO.preventDefault();
+  modalLogin.classList.remove('modal-show');
+  modalLogin.classList.remove('modal-error');
+}
+
+function closeLoginEscape(EO) {
+  if(EO.keyCode === 27) {  
+
+    if(modalLogin.classList.contains('modal-show')) {
+      EO.preventDefault();
+      modalLogin.classList.remove('modal-show');
+      modalLogin.classList.remove('modal-error');
+    }
+  }
+}
+
---
+++ js/map.js
@@ -0,0 +1,27 @@
+var map = document.querySelector('.contacts__map');
+var modalMap = document.querySelector('.modal-map');
+var modalMapClose = document.querySelector('.modal-map__close');
+
+map.addEventListener('click', showMap);
+modalMapClose.addEventListener('click', closeMap);
+window.addEventListener('keydown', closeMapEscape);
+
+function showMap(EO) {
+  EO.preventDefault();
+  modalMap.classList.add('modal-show');
+}
+
+function closeMap(EO) {
+  EO.preventDefault();
+  modalMap.classList.remove('modal-show');
+}
+
+function closeMapEscape(EO) {
+  if(EO.keyCode === 27) {  
+
+    if(modalMap.classList.contains('modal-show')) {
+      EO.preventDefault();
+      modalMap.classList.remove('modal-show');
+    }
+  }
+}
